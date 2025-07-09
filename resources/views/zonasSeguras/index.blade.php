c4f2b1ba353c5acddafbb9fcbc38b502bf957f7b
DeividCasa <deivid.casa0439@utc.edu.ec>
2025-07-09 17:03:08 -0500

    actualizando el index

--- resources/views/zonasSeguras/index.blade.php
+++ resources/views/zonasSeguras/index.blade.php
@@ -9,15 +9,15 @@
 
         <a class="btn btn-info" href="{{ route('zonasSeguras.create') }}">Agregar Zona Segura</a>  <br><br>
 
-        <table id="tblZnSeg" class="table table-hover table-bordered table-striped">
+        <table id="tblZnSeguras" class="table table-hover table-bordered table-striped">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Nombre</th>
                     <th>Radio</th>
-                    <th>Tipo de Seguridad</th>
                     <th>Latitud</th>
                     <th>Longitud</th>
+                    <th>Tipo de Seguridad</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
@@ -27,9 +27,9 @@
                     <td>{{ $zona->id }}</td>
                     <td>{{ $zona->nombre }}</td>
                     <td>{{ $zona->radio }} m</td>
-                    <td>{{ $zona->tipo_seguridad }}</td>
                     <td>{{ $zona->latitud }}</td>
                     <td>{{ $zona->longitud }}</td>
+                    <td>{{ $zona->tipo }}</td>
                     <td>
                         <a class="btn btn-warning btn-sm" href="{{ route('zonasSeguras.edit', $zona->id) }}">Editar</a>
                         <form action="{{ route('zonasSeguras.destroy', $zona->id) }}" method="POST" style="display:inline-block;">
@@ -49,11 +49,9 @@
 
 <script>
     $(document).ready(function () {
-        $('#tblZnSeg').DataTable({
+        $('#tblZnSeguras').DataTable({
             dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ],
+            buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
             language: {
                 url: 'https://cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
             }
