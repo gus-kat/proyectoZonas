6d5ef6964e4ae84f616c6c2cba0b29f2009f8615
DeividCasa <deivid.casa0439@utc.edu.ec>
2025-07-10 19:14:39 -0500

    titulo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <title>Puntos de Vuelo</title>
+    <title>Zonas </title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta content="" name="keywords" />
     <meta content="" name="description" />
@@ -117,7 +117,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top py-0 px-3 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
       <a href="index.html" class="navbar-brand p-0">
         <img class="img-fluid me-3"  src="{{ asset('backend/assets/img/icon/icon-10.png') }}" alt="Icon" />
-        <h1 class="m-0 text-primary">Vuelos</h1>
+        <h1 class="m-0 text-primary">Zonas</h1>
       </a>
        <a href=" {{route ('zonasRiesgo.index')}}" style="color:blue" class="nav-item nav-link active ">Inicio</a>
         <div class="nav-item dropdown">
