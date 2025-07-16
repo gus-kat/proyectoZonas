b0a6bbe7a0b5bb2b4800ddcce2755a7369646373
DeividCasa <deivid.casa0439@utc.edu.ec>
2025-07-16 11:50:11 -0500

    plantilla arreglada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,12 +102,7 @@
             <small class="fa fa-phone-alt text-primary me-2"></small>
             <small>0979359012</small>
           </div>
-          <div class="h-100 d-inline-flex align-items-center">
-            <a class="btn btn-sm-square bg-white text-primary me-1" href=""><i class="fab fa-facebook-f"></i></a>
-            <a class="btn btn-sm-square bg-white text-primary me-1" href=""><i class="fab fa-twitter"></i></a>
-            <a class="btn btn-sm-square bg-white text-primary me-1" href=""><i class="fab fa-linkedin-in"></i></a>
-            <a class="btn btn-sm-square bg-white text-primary me-0" href=""><i class="fab fa-instagram"></i></a>
-          </div>
+
         </div>
       </div>
     </div>
@@ -196,58 +191,23 @@
             <p class="mb-2">
               <i class="fa fa-phone-alt me-3"></i>0979359012
             </p>
-            <p class="mb-2">
-              <i class="fa fa-envelope me-3"></i>puntosVuelos.com
-            </p>
-            <div class="d-flex pt-2">
-              <a class="btn btn-outline-light btn-social" href=""
-                ><i class="fab fa-twitter"></i
-              ></a>
-              <a class="btn btn-outline-light btn-social" href=""
-                ><i class="fab fa-facebook-f"></i
-              ></a>
-              <a class="btn btn-outline-light btn-social" href=""
-                ><i class="fab fa-youtube"></i
-              ></a>
-              <a class="btn btn-outline-light btn-social" href=""
-                ><i class="fab fa-linkedin-in"></i
-              ></a>
-            </div>
-          </div>
+
+
           
-          <div class="col-lg-3 col-md-9">
-            <h5 class="text-light mb-4">Contactanos</h5>
-            <p>Envianos tu correo para contactarnos</p>
-            <div class="position-relative mx-auto" style="max-width: 400px">
-              <input
-                class="form-control border-0 w-100 py-3 ps-4 pe-5"
-                type="text"
-                placeholder="Tu Correo"
-              />
-              <button
-                type="button"
-                class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
-              >
-                Enviar
-              </button>
-            </div>
+
           </div>
         </div>
       </div>
       <div class="container">
         <div class="copyright">
           <div class="row">
-            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-              &copy; <a class="border-bottom" href="#">Puntos Turisticos del Ecuador</a>, Derechos Reservados.
-            </div>
+
             <div class="col-md-6 text-center text-md-end">
               <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
               Hecho Por
-              <a class="border-bottom" href="https://htmlcodex.com"
-                >HTML Codex</a
+              <a  
+                >RLS TEHC</a
               >
-              <br />Distributed By:
-              <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
             </div>
           </div>
         </div>
